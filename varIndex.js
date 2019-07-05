b1f05d41a3c188ae0a925c803ed157945088f5c3
ryley8302 <ryley8302@gmail.com>
2019-07-05 18:05:03 +0000

    Automatically backed up by Learn

---
+++ varIndex.js
@@ -0,0 +1,16 @@
+function add(x, y) {
+  return x + y
+}
+
+add(1, 2)
+
+add(80, 9000)
+
+function say(greeting,firstName){
+  console.log('I was callled!')
+  return `${greeting}, ${firstName}!`
+}
+
+say("Hello", "Sofia")
+
+say("Howdy", "partner")
